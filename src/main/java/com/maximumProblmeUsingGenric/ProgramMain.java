e2886857e04c0bb9493041b79adb27acb6242477
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-16 13:58:44 +0530

    Added:-Welcome Message

---
+++ src/main/java/com/maximumProblmeUsingGenric/ProgramMain.java
@@ -0,0 +1,10 @@
+package com.maximumProblmeUsingGenric;
+
+public class ProgramMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Maximum Program Using Genric");
+
+    }
+}
